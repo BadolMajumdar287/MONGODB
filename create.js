b966d9572f1b23d3bcc28e7562451818682015e6
BadolMajumdar287 <badolmajumdar2006@gmail.com>
2025-04-23 16:16:06 +0530

    commit to update database

--- create.js
+++ create.js
@@ -58,6 +58,132 @@ location: {
 
 
 
+db.createCollection("student",{
+
+    validator: {
+
+     $jsonSchema:{
+        
+     requird:["name","age"], title: "student Object validation",
+      properties:{
+        name:{
+            bsonType: "string",
+            description: "Name must be a string and is required"
+        },
+         
+        age:{
+            bsonType: "int",
+            minimum: 5,
+            maximum: 20,
+            description: "age must be an integer between 5 and 20."
+        }
+
+
+      }
+
+
+     }
+
+ }
+
+})
 
 
 
+
+
+
+
+
+
+db.createCollection("user",{
+     
+    validator:{
+      $jsonSchema:{
+       
+        title:"Student Object Validation",
+
+        required:["name","age","course"],
+
+        properties:{
+
+            name:{
+                bsonType:"string",
+                description:"Name must be string and requird"
+            },
+
+            age:{
+                bsonType:"int",
+                minimum:5,
+                maximum:20,
+                description:"Age must be int and requird"
+            },
+
+            course:{
+                bsonType:"string",
+                enum:["BCA","Btech","BSc"],
+                 description:"Course must be one of the following values: BCA,Btech,BSc."
+
+            }
+        }
+
+
+
+      }
+
+
+    }
+
+
+
+})
+
+
+db.user.insertOne({name: "badol",age:20,course:"BCA"});
+db.user.insertOne({name: "avi",age:18,course:"BCA"});
+db.user.insertOne({name: "Shomrat",age:19,course:"BCA"});
+
+db.user.find()
+
+
+
+
+
+
+
+
+
+db.runCommand({
+    collMod: "student",
+    validator: {
+        $jsonSchema: {
+             required: ["name","age","course"],
+             properties: {
+                name:{
+                    bsonType:"string",
+                    description:"Name must be string and requird"
+                },
+    
+                age:{
+                    bsonType:"int",
+                    minimum:5,
+                    maximum:20,
+                    description:"Age must be int and requird"
+                },
+    
+                course:{
+                    bsonType:"string",
+                    enum:["BCA","Btech","BSc"],
+                     description:"Course must be one of the following values: BCA,Btech,BSc."
+    
+                }
+             }
+        }
+    }
+})
+
+db.student.insertOne({name: "Monty",age:20,course:"BCA"})
+db.student.insertOne({name: "Kisor",age:20,course:"BCA"})
+
+
+db.student.find();
--- update.js
+++ update.js
@@ -6,3 +6,19 @@ db.student.updateOne(
       $currentDate: { lastModified: true }
     }
 )
+
+
+
+// updateOne();
+// UpdateMany();
+
+db.student.updateOne(
+  {name: "avi"},
+  {$set: {age : 20,name:"badol"}}
+)
+
+
+db.student.updateOne(
+  { _id: ObjectId('6808bab1d569ab9b4fb5f8a0')},
+  {$set: {age : 17,name:"Joy"}}
+)
